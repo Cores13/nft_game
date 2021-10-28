df8c15a0a9fb8c95a20d39585af19c5fab59d8c9
Amir Kasumovic <amir.kasumovic@live.com>
2021-10-28 12:15:12 +0200

    save

--- contracts/NFT_Token.sol
+++ contracts/NFT_Token.sol
@@ -1,6 +1,45 @@
 pragma solidity ^0.8.0;
 
-import "./ERC721.sol";
-import "./Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT_Token is ERC721, Ownable {}
+contract NFT_Token is ERC721, Ownable {
+  uint256 nextId = 0;
+
+  struct Pet {
+    uint8 damage;
+    uint8 magic;
+    uint256 lastMeal;
+    uint256 endurance;
+  }
+
+  mapping(uint256 => Pet) private _tokenDetails;
+
+  constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+  function mint(
+    uint8 _damage,
+    uint8 _magic,
+    uint8 _endurance
+  ) public onlyOwner {
+    _tokenDetails[nextId] = Pet(_damage, _magic, block.timestamp, _endurance);
+
+    _safeMint(msg.sender, nextId);
+    nextId++;
+  }
+
+  function feed(uint256 tokenId) public {
+    Pet storage pet = _tokenDetails[tokenId];
+    require(block.timestamp < pet.lastMeal + pet.endurance);
+    pet.lastMeal = block.timestamp;
+  }
+
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 tokenId
+  ) internal virtual {
+    Pet storage pet = _tokenDetails[tokenId];
+    require(block.timestamp < pet.lastMeal + pet.endurance); // Pet is still alive
+  }
+}
